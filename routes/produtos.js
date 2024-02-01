2b3b7cb87b903d96db8a0d42929c6e4edeefe268
joaoMdeAsilva <jmda363959@outlook.com>
2024-02-01 16:09:12 -0300

    feito o metodo put

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const routes = express.Router();
+let dadosProdutos = []
 
 // rota root
 routes.get('/', (req, res, next) =>{
     res.send({
-        mensagem: 'Seja bem vindo(a) a rota de produtos!'
+        mensagem: 'Seja bem vindo(a) a rota de produtos!',
+        rota: '/protudos/protudo_estoque',
+        query: [
+            'nome_produto',
+            'preco_produto',
+        ]
     });
 });
+
+// visualizar produtos em estoque
+routes.get('/protudo_estoque', (req, res, next) => {
+    res.status(200).send({produtos: dadosProdutos});
+})
+
+// rota para adicionar novos produtos
+routes.post('/', (req, res, next) => {
+    const {id, nome_produto, preco_produto} = req.query
+    
+    dadosProdutos.push({
+        id: id,
+        nome: nome_produto,
+        preco: preco_produto
+    })
+    
+    res.status(200).send({
+        mensagem: `Produto ${nome_produto} com preço de R$${preco_produto} foram adicionados.`,
+        rota: 'Coloque a rota /produtos/protudo_estoque para ver os produtos!',
+    })
+
+})
+
+// rota para editar o produto
+routes.put('/editar_produto', (req, res, next)=>{
+    const {id, nome_produto, preco_produto} = req.query
+    const index = dadosProdutos.findIndex((dados)=> dados.id == id)
+
+    nomeAnterior = dadosProdutos[index].nome
+
+    dadosProdutos[index].nome = nome_produto
+    dadosProdutos[index].preco = preco_produto
+    
+    res.status(201).send({
+        mensagem: `Nome do produto ${nome_produto} e preco atual R$${preco_produto} ` 
+    })
+})
+
+    
 
 // rota de produtos de materiais escolares
 routes.get('/materiais_escolares', (req, res, next) =>{
